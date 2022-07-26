7113b6547d9603778725d046bd8f65026e6d1a74
Laís Galvão <laisgbueno62@gmail.com>
2022-07-26 18:31:42 -0300

    feat: envio de emails

---
+++ resources/views/emails/sendUserData.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <meta content="width=device-width" name="viewport" />
+    <meta content="IE=9; IE=8; IE=7; IE=EDGE" http-equiv="X-UA-Compatible" />
+    <title>Márcio, um novo cliente entrou em contato</title>
+
+</head>
+
+<body style="background:#f5f4f2">
+    <div style="padding:0;margin:0 auto;font-family:Manrope,sans-serif">
+        <div
+            style="background-color:#332F27;width:95%;margin:10px auto;max-width:600px;-webkit-box-shadow: 0 0 5px 2px rgba(0, 0, 0, .1);box-shadow: 0 0 5px 2px rgba(0, 0, 0, .1);">
+
+            <img src="https://app.jade.shinier.com.br/img/LOGO_JADE.f0010ad1.png" alt="Logo AcquaNativa"
+                style="display:block;margin:0 auto;padding-top:32px; height:50px;">
+
+            <h2 style="color:#68A35D;text-align:center;">Dados:</h2>
+
+            <p style="margin-bottom:10px;color:#ffffff;text-align:center;padding:0 20px;"><span
+                    style="font-weight: 600">Nome:</span> <?php echo $info['dados']; ?></p>
+           {{--  <p style="margin-bottom:10px;color:#ffffff;text-align:center;padding:0 20px;"><span
+                    style="font-weight: 600">Documento:</span> <?php echo $info['cpf']; ?>
+            </p>
+            <p style="margin-bottom:10px;color:#ffffff;text-align:center;padding:0 20px;"><span
+                    style="font-weight: 600">Email:</span> <?php echo $info['email']; ?></p>
+            <p style="margin-bottom:10px;color:#ffffff;text-align:center;padding:0 20px;"><span
+                    style="font-weight: 600">Telefone:</span> <?php echo $info['telephone']; ?>
+            </p> --}}
+
+
+            <div style="display: block;text-align: center;padding: 40px 0;background-color: #000000;color: #fff;">
+
+            </div>
+            <div class="yj6qo"></div>
+            <div class="adL">
+            </div>
+        </div>
+        <div class="adL">
+        </div>
+    </div>
+</body>
+
+</html>
